68a329795f2f302427f6cf528ef22973c294d1fe
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-07 00:05:12 +0530

    MongoDB Installation and Setup

--- index.js
+++ index.js
@@ -34,3 +34,16 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// Mongoose setup
+const PORT = process.env.PORT || 6001;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} didn't connect`));
